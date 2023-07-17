8d25c42398d23e693644efb5388f3fbca2483186
Wawerugeek <stevemuiruri84@gmail.com>
2023-07-18 00:19:06 +0300

    square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -27,7 +27,7 @@ class square():
 
 if __name__ == "__main__":
     """creating a square object"""
-    s = square(width=12, height=9)
+    s = square(width=12, height=12)
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
