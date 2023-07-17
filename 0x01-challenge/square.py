f4c8a0b2b26c83d3e0230d4c042838518ad3e68b
Wawerugeek <stevemuiruri84@gmail.com>
2023-07-17 23:43:19 +0300

    change the input

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -22,7 +22,7 @@ class Square():
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-    s = Square(width=12, height=9)
+    s = Square(width=12, height=12)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
